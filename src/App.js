97b917fe356485245d6b666fb56d5cc256265c60
leandro fernandez aguilar <leandroferaguilar@hotmail.com>
2022-08-23 23:00:38 -0300

    Actualización

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Leandro Fernandez 
         </p>
+        <p>lf-luthier</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Link1</li>
+                <li>Link2</li>
+                <li>Link3</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
